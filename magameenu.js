c5077232533971805ed36dc75838c15ad1472f63
maneeshbudholiya <143099577+maneeshbudholiya@users.noreply.github.com>
2023-10-04 07:16:27 -0700

    navbar upload

---
+++ magameenu.js
@@ -0,0 +1,14 @@
+function mk()
+{
+    let dropdwoun=``;
+}
+function run()
+{
+    setTimeout(play,3000);
+    function play()
+    {
+        document.getElementById('mega').style.display="flex";
+        document.getElementById('firstlogo').style.display="none";
+
+    }
+}
